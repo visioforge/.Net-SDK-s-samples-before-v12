9be7cd16c474020f8e955bc559626bf22ce3e18c
visioforge <support@visioforge.com>
2019-02-08 00:32:21 +0300

    Update to SDK version 11.0.27

--- Media Player SDK/WinForms/CSharp/Two Windows/Properties/AssemblyInfo.cs
+++ Media Player SDK/WinForms/CSharp/Two Windows/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("11.0.24.0")]
-[assembly: AssemblyFileVersion("11.0.24.0")]
+[assembly: AssemblyVersion("11.0.27.0")]
+[assembly: AssemblyFileVersion("11.0.27.0")]
--- Video Capture SDK/WinForms/VB.Net/Main Demo/My Project/AssemblyInfo.vb
+++ Video Capture SDK/WinForms/VB.Net/Main Demo/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("11.0.24.0")>
-<Assembly: AssemblyFileVersion("11.0.24.0")>
+<Assembly: AssemblyVersion("11.0.27.0")>
+<Assembly: AssemblyFileVersion("11.0.27.0")>
