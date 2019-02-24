4d6094d7a6670fe35191d26a755a983528b1153a
visioforge <support@visioforge.com>
2019-02-24 17:16:30 +0300

    Update to SDK version 11.0.36

--- Media Player SDK/WinForms/CSharp/Audio Player/Properties/AssemblyInfo.cs
+++ Media Player SDK/WinForms/CSharp/Audio Player/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("11.0.35.0")]
-[assembly: AssemblyFileVersion("11.0.35.0")]
+[assembly: AssemblyVersion("11.0.36.0")]
+[assembly: AssemblyFileVersion("11.0.36.0")]
