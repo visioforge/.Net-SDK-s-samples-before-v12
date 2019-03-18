99f0560e9dddaa6bb0c2e8b82039bb8ae6bc2eb2
visioforge <support@visioforge.com>
2019-03-18 17:54:26 +0300

    Update to SDK version 11.0.47

--- Media Player SDK/WinForms/CSharp/Memory Stream/Properties/AssemblyInfo.cs
+++ Media Player SDK/WinForms/CSharp/Memory Stream/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("11.0.46.0")]
-[assembly: AssemblyFileVersion("11.0.46.0")]
+[assembly: AssemblyVersion("11.0.47.0")]
+[assembly: AssemblyFileVersion("11.0.47.0")]
