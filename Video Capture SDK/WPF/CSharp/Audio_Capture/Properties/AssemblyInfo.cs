08fae19523c086c14347a8f57ab7e2eeaea10b7f
visioforge <support@visioforge.com>
2019-04-10 19:44:26 +0300

    Update to SDK version 11.0.62

--- Video Capture SDK/WPF/CSharp/Audio_Capture/Properties/AssemblyInfo.cs
+++ Video Capture SDK/WPF/CSharp/Audio_Capture/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("11.0.61.0")]
-[assembly: AssemblyFileVersion("11.0.61.0")]
+[assembly: AssemblyVersion("11.0.62.0")]
+[assembly: AssemblyFileVersion("11.0.62.0")]
