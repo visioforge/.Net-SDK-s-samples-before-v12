5039273d8c8fb8bc563b6d185d961ceed002ab35
visioforge <support@visioforge.com>
2019-04-23 22:57:47 +0300

    Update to SDK version 11.1.2

--- Video Capture SDK/WinForms/CSharp/IP Capture/Properties/AssemblyInfo.cs
+++ Video Capture SDK/WinForms/CSharp/IP Capture/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("11.1.0.0")]
-[assembly: AssemblyFileVersion("11.1.0.0")]
+[assembly: AssemblyVersion("11.1.2.0")]
+[assembly: AssemblyFileVersion("11.1.2.0")]
