920d99e14aabc448245fa6e0ca9f3419088345b0
visioforge <support@visioforge.com>
2019-06-28 11:23:47 +0300

    Update to SDK version 11.1.35

--- Media Player SDK/WinForms/VB .Net/Audio Player/My Project/AssemblyInfo.vb
+++ Media Player SDK/WinForms/VB .Net/Audio Player/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("11.1.34.0")>
-<Assembly: AssemblyFileVersion("11.1.34.0")>
+<Assembly: AssemblyVersion("11.1.35.0")>
+<Assembly: AssemblyFileVersion("11.1.35.0")>
