9ac1e9a542d7343f132e0b00ce172494d29160d1
visioforge <support@visioforge.com>
2019-06-29 18:31:50 +0300

    Update to SDK version 11.1.38

--- Video Edit SDK/WinForms/VB.Net/Main Demo/My Project/AssemblyInfo.vb
+++ Video Edit SDK/WinForms/VB.Net/Main Demo/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("11.1.37.0")>
-<Assembly: AssemblyFileVersion("11.1.37.0")>
+<Assembly: AssemblyVersion("11.1.38.0")>
+<Assembly: AssemblyFileVersion("11.1.38.0")>
