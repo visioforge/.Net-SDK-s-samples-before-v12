691eaeb07d168921dbd7e780a49ff5f99443bf09
visioforge <support@visioforge.com>
2019-07-03 13:15:54 +0300

    Update to SDK version 11.1.40

--- Media Player SDK/WinForms/CSharp/DVD Player/Properties/AssemblyInfo.cs
+++ Media Player SDK/WinForms/CSharp/DVD Player/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("11.1.39.0")]
-[assembly: AssemblyFileVersion("11.1.39.0")]
+[assembly: AssemblyVersion("11.1.40.0")]
+[assembly: AssemblyFileVersion("11.1.40.0")]
